8f0a2a262743aa6578d589684b4da73cde6318f6
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2023-10-14 23:48:29 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+result = 0
+for _ in range (b):
+    c,d = map(int, input().split())
+    result += c * d
+
+if(a == result):
+    print("Yes")
+else: 
+    print("No")
