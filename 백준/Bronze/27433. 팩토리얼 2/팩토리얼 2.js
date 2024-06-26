0999a6d957f5c8ecd1378a2c246e9f5c2163ef6f
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-06-27 00:04:29 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 92 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.js
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync('dev/stdin'));
 
-let sum = 1;
-
-if (n >= 1) {
-
-  for (let i = 1; i <= n; i++) {
-    sum *= i;
+function count(num) {
+  if (num <= 1) {
+    return 1;
   }
-  console.log(sum);
+  return num * count(num - 1)
 }
 
-else { console.log(sum); }
+console.log(count(n));
