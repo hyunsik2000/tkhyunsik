de5f283b68b07fbea35d686beb9a65e9a4741186
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-09-23 21:44:17 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -3,8 +3,5 @@ function solution(price, money, count) {
     for(let i = 1; i <= count; i++){
         money -= price * i;
     }
-    if (money < 0){
-        return money * -1;
-    }
-    else {return 0;}
+    return money < 0 ? money * -1: 0;
 }
