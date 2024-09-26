25f71412ae9040a7a588a18906b9c6b7bfdb83b1
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-09-26 22:47:03 +0900

    [level 1] Title: 대충 만든 자판, Time: 33.72 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,24 @@
+function solution(keymap, targets) {
+    var answer = [];
+    for (let i = 0; i < targets.length;i++){   
+        answer.push(0);
+        for (let j = 0; j < targets[i].length; j++){
+            let m = 0;
+            let n = [];
+            while (m !== keymap.length){
+                if(keymap[m].includes(targets[i][j])){
+                n.push(keymap[m].indexOf(targets[i][j]) + 1);
+                }
+                m++
+            }
+            if (n.length === 0){
+                answer[i] = -1;
+                break;
+            }
+            n.sort((a,b) => (a - b));
+            answer[i] += n[0];
+        }
+    }
+    
+    return answer;
+}
