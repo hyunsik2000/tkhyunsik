76595d5be8276352328541ed7eff8001fd35d729
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-10-04 19:49:10 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 62.27 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,7 +1,7 @@
 function solution(t, p) {
     var answer = 0;
     for(let i = 0; i < t.length + 1 - p.length; i++){
-        if(parseInt(t.slice(i,i + p.length)) <= parseInt(p)){
+        if(t.slice(i,i + p.length) <= p){
             console.log(t.slice(i,i+p.length));
             answer++;
         }
