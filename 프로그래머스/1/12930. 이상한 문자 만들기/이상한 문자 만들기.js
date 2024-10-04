2ad81aff61d136dfa03743b521e3291764ec4510
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-10-04 20:13:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -3,13 +3,8 @@ function solution(s) {
     let newS = s.split(' ');
     for (let i = 0; i < newS.length; i++){
         let newWord = '';
-        for(let j = 0; j < newS[i].length; j++){
-            if(j % 2 !== 0){
-                newWord += newS[i][j].toLowerCase();
-            }
-            else {
-                newWord += newS[i][j].toUpperCase();
-            }
+        for (let j = 0; j < newS[i].length; j++) {
+            newWord += (j % 2 !== 0) ? newS[i][j].toLowerCase() : newS[i][j].toUpperCase();
         }
         answer += newWord;
         if (i < newS.length - 1) {
