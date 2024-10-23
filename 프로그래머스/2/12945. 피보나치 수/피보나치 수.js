504b0e73c9b073ce639c9dcbce40abce4d2ad172
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-10-23 22:29:55 +0900

    [level 2] Title: 피보나치 수, Time: 7.66 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    const MOD = 1234567;
+    let fib = [0, 1];
+    
+    for (let i = 2; i <= n; i++) {
+        fib[i] = (fib[i-1] + fib[i-2]) % MOD;
+    }
+    
+    return fib[n];
+}
