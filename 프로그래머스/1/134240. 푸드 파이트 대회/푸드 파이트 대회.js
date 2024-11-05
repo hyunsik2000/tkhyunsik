d4b80fcaa7c9efc22e5a517dc3f33d93cb38e6e5
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-11-05 22:08:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,16 +1,8 @@
 function solution(food) {
     var answer = '';
     for(let i = 1; i < food.length; i++){
-        let char = food[i];
-        if(food[i] % 2 === 1 && food[i] > 1){
-            food.splice(i,1,food[i]-1);
-        }
-        answer += i.toString().repeat(char / 2);
+        answer += i.toString().repeat(Math.floor(food[i] / 2));
     }
-    answer += "0";
-    for(let i = food.length; i > 0; i--){
-        let char = food[i];
-        answer += i.toString().repeat(char / 2);
-    }
+    answer = answer + "0" + [...answer].reverse().join('');
     return answer;
 }
