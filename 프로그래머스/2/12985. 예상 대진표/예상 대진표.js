481b0f3eb1bec28c8c5d43d1b967ec4a3028a580
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-11-25 19:39:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,19 +1,13 @@
-function solution(n,a,b)
-{
-    var answer = 1;
+function solution(n, a, b) {
+    var answer = 0;
     var numA = a;
     var numB = b;
-    for(n;n > 1;n /= 2){
-        let guessNum = numA - numB;
-        if(numA % 2 == 1 && guessNum == -1 || numA % 2 == 0 && guessNum == 1){
-            break;
-        } else
-        {
-            numA = Math.ceil(numA / 2);
-            numB = Math.ceil(numB / 2);
-            console.log(numA);
-        }
+    
+    while (numA !== numB) {
+        numA = Math.ceil(numA / 2);
+        numB = Math.ceil(numB / 2);
         answer++;
     }
+    
     return answer;
 }
