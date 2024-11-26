8be1129559d0b3024bb6babc85fcf1e88612b65d
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-11-26 23:56:59 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 991.55 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,17 @@
+function solution(elements) {
+  const n = elements.length;
+  const extendedElements = [...elements, ...elements];
+  const sums = new Set();
+
+  for (let length = 1; length <= n; length++) {
+    for (let start = 0; start < n; start++) {
+      let sum = 0;
+      for (let i = 0; i < length; i++) {
+        sum += extendedElements[start + i];
+      }
+      sums.add(sum);
+    }
+  }
+
+  return sums.size;
+}
