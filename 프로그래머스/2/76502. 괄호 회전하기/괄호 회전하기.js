3d3600405a204e4bbcbfad7e28817d1f8b8e0fb2
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-12-03 23:30:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 23.26 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,28 +1,28 @@
 function solution(s) {
- 
-    function isValid(str) {
-        const stack = []; 
-        const matching = { '(': ')', '[': ']', '{': '}' };
-
-        for (let char of str) {
-            if (char === '(' || char === '[' || char === '{') {
+    function isValid(str){
+        var stack = [];
+        var match = {"(" : ")", "[" : "]", "{" : "}"};
+       
+        for(let char of str){
+            if(char === "(" || char === "[" || char === "{"){
                 stack.push(char);
             } else {
-                if (stack.length === 0 || matching[stack.pop()] !== char) {
+                if(stack.length === 0 || match[stack.pop()] !== char){
                     return false;
                 }
             }
         }
         return stack.length === 0;
     }
-    let count = 0;
-    let len = s.length;
     
-    for (let i = 0; i < len; i++) {
-        let rotated = s.slice(i) + s.slice(0, i); 
-        if (isValid(rotated)) {
-            count++; 
+    var count = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        let newStr = s.slice(i) + s.slice(0,i);
+        if(isValid(newStr)){
+            count++;
         }
     }
-    return count; 
+    
+    return count;
 }
