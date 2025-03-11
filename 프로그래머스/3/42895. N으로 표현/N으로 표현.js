fd627573167e0091714d4273dfd0ab4fb03d75ec
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-03-11 10:57:48 +0900

    [level 3] Title: N으로 표현, Time: 19.40 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -0,0 +1,22 @@
+function solution(N, number) {
+    var answer = 0;
+    
+    const dp = Array.from({ length : 9 }, () => new Set());
+    
+    for(let i = 1; i < 9; i++){
+        dp[i].add(Number(String(N).repeat(i)));
+        
+        for(let j = 1; j < i; j++){
+            for (const a of dp[j]) {
+                for (const b of dp [i - j]){
+                    dp[i].add(a + b);
+                    dp[i].add(a - b);
+                    dp[i].add(a * b);
+                    dp[i].add(Math.floor(a / b));
+                }
+            }
+        }
+        if (dp[i].has(number)) return i;
+    }
+    return -1;
+}
