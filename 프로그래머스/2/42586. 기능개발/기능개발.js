0e0419d3c834d5040490362362227a9401876c20
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-03-17 12:53:52 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,20 +1,26 @@
+/*
+94 60 60
+95 90 65
+96 120 70
+*/
 function solution(progresses, speeds) {
-    const days = progresses.map((progress,i) => Math.ceil((100 - progress) / speeds[i]));
-    const answer = [];
-
-    let currentMax = days[0];
+    var answer = [];
+    let days = progresses.map((progress, index) => 
+        Math.ceil((100 - progress) / speeds[index])
+    );
     let count = 1;
+    let maxDays = days[0];
     
-    for(let i = 1; i < days.length; i++){
-        if(days[i] > currentMax){
-            currentMax = days[i];
-            answer.push(count);
-            count = 1;
-        } else {
+    for(let i = 1; i < days.length;i++){
+        if(days[i] <= maxDays){
             count++;
         }
+        else {
+            answer.push(count);
+            maxDays = days[i];
+            count = 1;
+        }
     }
-    
     answer.push(count);
     return answer;
 }
