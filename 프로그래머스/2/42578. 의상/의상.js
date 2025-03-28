23881e146daf6b57b6c2250d11911ce6de854139
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-03-28 16:14:49 +0900

    [level 2] Title: 의상, Time: 3.97 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,26 @@
 function solution(clothes) {
     var answer = 0;
-    var parts = {};
+    let newClothes = new Map();
     
-    for (let i = 0; i < clothes.length;i++){
-        const [name, type] = clothes[i];
-        parts[type] = (parts[type] || 0) + 1;
+    for(let i = 0; i < clothes.length; i++){
+        let kind = clothes[i][1];
+        if(!newClothes.has(kind)){
+            newClothes.set(kind,1);
+            answer++;
+        }
+        else {
+            let num = newClothes.get(kind);
+            newClothes.set(kind, num+1);
+        }
     }
     
-    let result = 1;
-    for (const type in parts) {
-        result *= (parts[type] + 1);
+    console.log(newClothes.size)
+    
+    let combinations = 1;
+    for (let count of newClothes.values()) {
+        combinations *= (count + 1);
     }
-    return result - 1;
+
+    
+    return newClothes.size === 1 ? clothes.length : combinations - 1;
 }
