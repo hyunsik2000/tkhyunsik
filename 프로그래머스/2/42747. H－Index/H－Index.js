fd028845d3e9847b31e9ede0e0f131e11a286217
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-04-01 12:01:38 +0900

    [level 2] Title: H-Index, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,12 +1,16 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a);
-    let hIndex = 0;
-    for (let i = 0; i < citations.length; i++) {
-        if (citations[i] >= i + 1) {
-            hIndex = i + 1;
-        } else {
-            break;
+    var answer = 0;
+    
+    citations.sort((a,b) => b - a);
+
+    for(let i = 0; i < citations.length; i++){
+        if(answer < citations[i]){
+            answer++;
+        }
+        else if(answer === citations[i]){
+            return answer;
         }
     }
-    return hIndex;
+    
+    return answer;
 }
