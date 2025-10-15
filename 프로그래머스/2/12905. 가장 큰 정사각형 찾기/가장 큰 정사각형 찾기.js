513c649296379b4433cbf4b4c801a2601ceccca7
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-15 19:42:23 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 24.00 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -8,6 +8,8 @@
 그럼 일단 max 값을 설정해서 저장 해야하나? 
 만약에 각 배열의 0번째가 1이 쭈루룩 나왔어 1단계 조건 완수 -> 2단계 각 배열의 그 길이 값 만큼 연속된 수가 1인가?
 --> 이게 뭔가 접근이 쉽고 정확한듯
+
+--> DP를 통해서 연결되어 있는 길이중 가장 큰 값을 구하는 것이기 때문에 결국에는 이 방법을 몰랐으면 노가다 였을듯 하다
 */
 
 function solution(board)
@@ -19,9 +21,8 @@ function solution(board)
     for(let i = 1; i < board.length; i++) {
         for(let j = 1; j < board[i].length; j++) {
             if(board[i][j] !== 0){
-                board[i][j] = Math.min(board[i-1][j-1], board[i][j-1], board[i-1][j])
-                board[i][j] += 1;
-                        max = Math.max(max, board[i][j])
+                board[i][j] = Math.min(board[i-1][j-1], board[i][j-1], board[i-1][j]) + 1;
+                max = Math.max(max, board[i][j])
             }
         }
     }
