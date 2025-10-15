d190d26acadb100edbd3a1ebab9add94fceec687
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-15 18:43:22 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 49.99 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -0,0 +1,30 @@
+/*
+    0 1 1 1
+    1 1 1 1
+    1 1 1 1
+    0 0 1 0
+
+그렇다면 각 1 2 3 4 에서 연속으로 1이 나오는 부분을 일단 찾기 -> 0 이 나오면?
+그럼 일단 max 값을 설정해서 저장 해야하나? 
+만약에 각 배열의 0번째가 1이 쭈루룩 나왔어 1단계 조건 완수 -> 2단계 각 배열의 그 길이 값 만큼 연속된 수가 1인가?
+--> 이게 뭔가 접근이 쉽고 정확한듯
+*/
+
+function solution(board)
+{
+    let max = 0;
+    
+    if(board.length === 1 || board[0].length === 0) return 1;
+
+    for(let i = 1; i < board.length; i++) {
+        for(let j = 1; j < board[i].length; j++) {
+            if(board[i][j] !== 0){
+                board[i][j] = Math.min(board[i-1][j-1], board[i][j-1], board[i-1][j])
+                board[i][j] += 1;
+                        max = Math.max(max, board[i][j])
+            }
+        }
+    }
+    
+    return max * max;
+}
