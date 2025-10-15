99f4ed61904ed81166dfcef0cb27d78c761ea327
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-15 15:56:08 +0900

    [level 1] Title: 하샤드 수, Time: 3.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,8 +1,8 @@
 function solution(x) {
     var answer = 0;
-    let m = x.toString();
+    let m = String(x)
     for (let i = 0; i < m.length; i++){
-        answer += parseInt(m[i]);
+        answer += Number(m[i]);
         console.log(m[i]);
     }
     if(x % answer === 0){
