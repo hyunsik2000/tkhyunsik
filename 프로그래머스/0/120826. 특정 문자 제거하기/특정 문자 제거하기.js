6a7a0ef9f3695d0b0e2e6bd05e81bdc3728ba7d1
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-16 12:58:34 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,7 @@
+function solution(my_string, letter) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++){
+        if(my_string[i] !== letter) answer += my_string[i]
+    }
+    return answer;
+}
