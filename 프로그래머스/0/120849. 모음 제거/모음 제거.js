31788dfe613f1b8c82a3f64674c52a2fe421cab9
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-16 13:22:38 +0900

    [level 0] Title: 모음 제거, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,13 +1,3 @@
 function solution(my_string) {
-    let answer = ""
-    let newString = my_string.split('')
-    for(let i = 0; i < newString.length; i++){
-        if(newString[i] !== "a" && newString[i] !== "e" && 
-           newString[i] !== "i" && newString[i] !== "o" && 
-           newString[i] !== "u") {
-            answer += newString[i]
-        }
-    }
-  
-    return answer;
+    return Array.from(my_string).filter(t => !['a','e','i','o','u'].includes(t)).join('');
 }
