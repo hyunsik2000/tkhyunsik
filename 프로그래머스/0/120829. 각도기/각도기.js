68ddeb39cb6984c9dadb9ca85de2da35f1c1938a
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-17 20:24:17 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,13 +1,5 @@
 function solution(angle) {
-    let answer = 0;
-    if(0 < angle && angle < 90){
-        answer = 1
-    } else if(angle == 90){
-        answer = 2
-    } else if(angle > 90 && angle < 180){
-        answer = 3
-    } else if(angle == 180){
-        answer = 4
-    }
-    return answer;
+
+    
+    return 0 < angle && angle < 90 ? 1 : angle == 90 ? 2 : 90 < angle && angle < 180 ? 3 : 4
 }
