a06986aec3603a5568a58f6da0fccd5876e00e20
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-20 18:25:10 +0900

    [level 1] Title: 소수 만들기, Time: 4.98 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,34 @@
+/*
+
+    7 11 13 17 19 23
+
+    배열에서 요소 3개 선택 해서 더해서 sum 설정 -> 배열에서 3개 어떻게 찾을건데...
+    소수인지 판별 -> sum % (2부터 sum -1) !== 0 이면 count++;
+
+*/
+
+
+function solution(nums) {
+    var answer = 0;
+
+    function isPrime(n) {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  const limit = Math.floor(Math.sqrt(n));
+  for (let d = 3; d <= limit; d += 2) {
+    if (n % d === 0) return false;
+  }
+  return true;
+}
+    for(let i = 0; i < nums.length - 2; i++){
+        for(let j = i + 1; j < nums.length - 1; j++){
+            for(let k = j + 1; k < nums.length; k++){
+                let sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)) answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
