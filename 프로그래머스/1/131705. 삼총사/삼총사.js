2574671078a7ed3d0ff40e6cbacab8bddbf2dccc
HyunSik Lyu <126768512+hyunsik2000@users.noreply.github.com>
2025-10-21 11:28:18 +0900

    [level 1] Title: 삼총사, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,6 +1,5 @@
 function solution(number) {
     var answer = 0;
-    number.sort((a,b) => (a - b));
     for (let i = 0; i < number.length - 2; i++){
         let sum = 0;
         for(let j = i + 1; j < number.length - 1; j++){
